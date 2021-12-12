2d207d42431c7fa81c6186babbd3c6fc46188091
Taniacardenas <tania.cardenas103@gmail.com>
2021-12-12 01:38:27 -0500

    Se agrega los dos metodos(list y detail) de Genre

---
+++ src/controllers/genresController.js
@@ -0,0 +1,17 @@
+const db = require('../database/models')
+
+const genresController={
+    list:(req,res)=>{
+        db.genres.findAll()
+        .then(function(genres){
+            res.render('genresList',{genres:genres})
+        })
+    },
+    detail:(req,res)=>{
+        db.genres.findByPk(req.params.id)
+        .then(function(genre){
+            res.render('genresDetail',{genre:genre})
+        })
+    },
+}
+module.exports= genresController
